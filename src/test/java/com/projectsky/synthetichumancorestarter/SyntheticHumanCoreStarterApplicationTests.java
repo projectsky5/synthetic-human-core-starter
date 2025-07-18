844ac9bcb35287bdd29e4ea7b39edebf9410bac9
projectsky5 <mackvej@gmail.com>
2025-07-18 12:49:51 +0300

    Initial commit

---
+++ src/test/java/com/projectsky/synthetichumancorestarter/SyntheticHumanCoreStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsky.synthetichumancorestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticHumanCoreStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
